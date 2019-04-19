1a943535e5160844e8a2e093193de696f4ac83c8
Walker <walker@praiseweb.com.br>
2019-04-18 22:36:50 -0300

    add loader

--- main.js
+++ main.js
@@ -1,30 +1,6 @@
-import render from './lib/render.js'
-import soldier from './lib/soldier/index.js'
-
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
-const reqFrame = window.requestAnimationFrame
-
-const assets = [
-    soldier.load()
-]
-
-function nextFrame() {
-    // get user input
-    // getUserInput()
-
-    // process one frame
-    // updateGame()
-    const entities = [
-        soldier.create(),
-    ]
-
-    // draw everything on the screen
-    ctx.clearRect(0, 0, 640, 480)
-    render({entities, assets, draw: ctx.drawImage});
-
-    // wait until a frame's worth of time has elapsed
-    reqFrame(nextFrame)
-}
-
-nextFrame(nextFrame)
+(async function() {
+    const canvas = document.querySelector('canvas')
+    const requestFrame = window.requestAnimationFrame
+    const game = await createGame({assets, canvas, requestFrame})
+    game.resume()
+})();
