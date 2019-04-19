b347e7a54edf9212c0147a02fd1714bde8926a51
Walker <walker@praiseweb.com.br>
2019-04-18 21:43:05 -0300

    add main loop

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+import render from './lib/render'
+
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+const reqFrame = window.requestAnimationFrame
+
+function nextFrame() {
+    // get user input
+    // getUserInput()
+
+    // process one frame
+    // updateGame()
+
+    // draw everything on the screen
+    ctx.clearRect(0, 0, 640, 480)
+    render({entities, assets, draw: ctx.drawImage});
+
+    // wait until a frame's worth of time has elapsed
+    reqFrame(nextFrame)
+}
+
+nextFrame(nextFrame)
