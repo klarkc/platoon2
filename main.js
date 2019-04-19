9ae2e60bab4366e13c0f9e10d7d8aaa69926e06f
Walker <walker@praiseweb.com.br>
2019-04-18 22:42:33 -0300

    fix undefined

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ import createGame from './lib/game.js'
 (async function() {
     const canvas = document.querySelector('canvas')
     const requestFrame = window.requestAnimationFrame
-    const game = await createGame({assets, canvas, requestFrame})
+    const game = await createGame({canvas, requestFrame})
     game.resume()
 })();
