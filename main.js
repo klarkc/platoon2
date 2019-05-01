08f640ffb531d8c1066775de781637c9247a2a40
Walker <walker@praiseweb.com.br>
2019-04-30 21:02:18 -0300

    turn engine pure fn

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import buildGame from './lib/game.js'
 import buildEngine from './lib/engine.js';
 
-(async function() {
-    const engine = buildEngine()
-    const game = await buildGame({engine})
+(async function(window) {
+    const engine = buildEngine({ type: 'web', window })
+    const game = await buildGame({ engine })
     game.resume()
-})();
+})(window);
